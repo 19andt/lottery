7442256ce7597e35d1cc748d0710656f99d76afd
Anand T <19andt@gmail.com>
2023-01-08 18:06:15 +0530

    Slot Model

--- src/satta/models.py
+++ src/satta/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+
+class IntegerRangeField(models.IntegerField):
+    def __init__(self, verbose_name=None, name=None, min_value=None, max_value=None, **kwargs):
+        self.min_value, self.max_value = min_value, max_value
+        models.IntegerField.__init__(self, verbose_name, name, **kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {'min_value': self.min_value, 'max_value':self.max_value}
+        defaults.update(kwargs)
+        return super(IntegerRangeField, self).formfield(**defaults)
+
+
 # Create your models here.
 class User(models.Model):
     name = models.CharField(
@@ -21,4 +33,25 @@ class User(models.Model):
     password = models.CharField(
         max_length=255,
         null=False
+    )
+
+
+class Slot(models.Model):
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+        null=False
+    )
+    order = models.IntegerField(
+        unique=True,
+        null=False
+    )
+    time_of_day = models.TimeField(
+        auto_now=True,
+        null=False
+    )
+    result_delta = IntegerRangeField(
+        default=0,
+        min_value=0,
+        max_value=299
     )
