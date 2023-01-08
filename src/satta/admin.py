05c6a93d9b68937599553da0c59dd47715be1174
Anand T <19andt@gmail.com>
2023-01-08 18:21:48 +0530

    Registering models

--- src/satta/admin.py
+++ src/satta/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import User, Slot, Bid, Game
 
 # Register your models here.
+
+
+@admin.register(User)
+class RequestDemoAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in User._meta.get_fields()]
+
+
+@admin.register(Slot)
+class RequestDemoAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Slot._meta.get_fields()]
+
+
+@admin.register(Bid)
+class RequestDemoAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Bid._meta.get_fields()]
+
+
+@admin.register(Game)
+class RequestDemoAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Game._meta.get_fields()]
