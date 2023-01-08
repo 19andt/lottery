bf4ff99a970aaba1f47fb89bc24537f8e57d5097
Anand T <19andt@gmail.com>
2023-01-08 18:04:16 +0530

    User Model

--- src/satta/models.py
+++ src/satta/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(
+        max_length=255,
+        null=False
+    )
+    user_type = models.CharField(
+        max_length=255,
+        null=False
+    )
+    mobile_number = models.IntegerField(
+        null=False
+    )
+    user_id = models.CharField(
+        max_length=255,
+        unique=True,
+        null=False
+    )
+    password = models.CharField(
+        max_length=255,
+        null=False
+    )
